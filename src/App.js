7010edb1c22e6f962ec6e9dfc723d77731e99a08
Jessica Pope <jessica.pope.co@gmail.com>
2019-10-01 12:00:23 -0600

    dynamic routing

--- src/App.js
+++ src/App.js
@@ -4,6 +4,30 @@ import './App.css';
 import IndexPage from './indexPage.js';
 import AboutPage from './aboutPage.js';
 
+const users = [
+  {
+    name: `Param`,
+  },
+  {
+    name: `Vennila`,
+  },
+  {
+    name: `Afrin`,
+  },
+];
+const UsersPage =() => {
+  return (
+    <>
+    <h3>Users Page</h3>
+    {users.map((user, index)=> (
+      <h5 key={index}>
+        <Link to={`/user/${index + 1}`}>{user.name}'s Page</Link>
+      </h5>
+    ))}
+    </>
+  )
+}
+
 const App = () => {
   return (
     <div className="App">
@@ -13,8 +37,10 @@ const App = () => {
     <Router>
       <Link to="/">Home</Link>
       <Link to="/about">About</Link>
-      <Route path="/" component={IndexPage}/>
-      <Route path="/about" component={AboutPage}/>
+      <Link to="/users">Users</Link>
+      <Route exact path="/" component={IndexPage}/>
+      <Route exact path="/about" component={AboutPage}/>
+      <Route exact path="/users" component={UsersPage}/>
     </Router>
 
     </div>
